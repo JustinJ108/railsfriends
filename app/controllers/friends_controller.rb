a0b2361ff3a264b40ee6d1a457a8c9fc2f3c0ef1
Justin Read <jread108@gmail.com>
2022-08-03 14:57:34 -0700

    F - this is the end of #9

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,6 +1,8 @@
 class FriendsController < ApplicationController
   before_action :set_friend, only: %i[ show edit update destroy ]
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+
 
   # GET /friends or /friends.json
   def index
@@ -13,7 +15,8 @@ class FriendsController < ApplicationController
 
   # GET /friends/new
   def new
-    @friend = Friend.new
+    # @friend = Friend.new
+    @friend = current_user.friends.build
   end
 
   # GET /friends/1/edit
@@ -22,7 +25,8 @@ class FriendsController < ApplicationController
 
   # POST /friends or /friends.json
   def create
-    @friend = Friend.new(friend_params)
+    # @friend = Friend.new(friend_params)
+    @friend = current_user.friends.build(friend_params)
 
     respond_to do |format|
       if @friend.save
@@ -57,6 +61,12 @@ class FriendsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def correct_user
+    @friend = current_user.friends.find_by(id: params[:id])
+    redirect_to friends_path, notice: "Not authorised to edit this friend" if @friend.nil?
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
